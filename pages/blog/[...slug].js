864dd3324bd82580ad579be8d768bd39bf66409a
Geomar Bastiani <mail@geomarbastiani.com>
2022-05-26 17:02:26 +0100

    blog

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+export default function BlogPostPage() {
+  const router = useRouter();
+
+  console.log(router.query);
+  return (
+    <div>
+      <h1>The Blog Posts Page</h1>
+    </div>
+  );
+}
