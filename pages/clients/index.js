c70ca14e7709fa81268cc5da1bdfb29a4ee374e4
Geomar Bastiani <mail@geomarbastiani.com>
2022-05-26 17:16:51 +0100

    navigate link string

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    { id: "geo", name: "Geomar Bastiani" },
+    { id: "max", name: "Maximilian" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
 export default function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
