a14334fa701df1c79eb02c5bc164f331a3f940ed
Geomar Bastiani <mail@geomarbastiani.com>
2022-05-26 16:49:04 +0100

    clients

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function SelectedClientProjectPage() {
+  const router = useRouter();
+  console.log(router.query);
+  return (
+    <div>
+      <h1>The Project Page for a Specific Project for a Selected Client</h1>
+    </div>
+  );
+}
