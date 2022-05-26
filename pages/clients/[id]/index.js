2c8cd8febc818a89c1993167e826b0eebf92f906
Geomar Bastiani <mail@geomarbastiani.com>
2022-05-26 17:24:10 +0100

    navigate prj id button string

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,10 +2,16 @@ import { useRouter } from "next/router";
 
 export default function ClientProjectPage() {
   const router = useRouter();
-  console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...pageProps
+    router.push("/clients/geo/projecta");
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 }
