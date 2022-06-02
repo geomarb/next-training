88260f094868240d8bbb06714b579163bf19fa5c
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-02 17:59:51 +0100

    feedback item route

---
+++ pages/api/[feedbackId].js
@@ -0,0 +1,11 @@
+import { getFeedbackData } from "./feedback";
+
+export default function handler(req, res) {
+  const { feedbackId } = req.query;
+  const feedbackData = getFeedbackData();
+  const selectedFeedback = feedbackData.find(
+    (feedback) => feedback.id === feedbackId
+  );
+
+  res.status(200).json({ feedback: selectedFeedback });
+}
