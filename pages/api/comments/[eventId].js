59567b5698da4f764007aea71eb556f6ec4fa1a3
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-03 17:27:11 +0100

    add get comments

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -24,20 +24,19 @@ export default async function handler(req, res) {
     const db = client.db("events");
     const result = await db.collection("comments").insertOne(comment);
 
-    console.log(result);
-
     res.status(201).json({ message: "Added Comment", comment, result });
     return;
   }
 
   if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Geo", text: "A first comment" },
-      { id: "c2", name: "Max", text: "A secondcomment" },
-    ];
+    const db = client.db("events");
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
 
-    res.status(200).json({ comments: dummyList });
+    res.status(200).json({ comments: documents });
   }
-
   client.close();
 }
