1bf1ed61da971a811e3d1e590c75dd0a57ba8866
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-03 17:06:54 +0100

    add mongodb

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,11 +1,22 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   console.log("route reached");
   if (req.method === "POST") {
     const { email } = req.body;
     if (!email || !email.includes("@") || !email.includes(".")) {
       return res.status(422).json({ message: "Inavlid email address." });
     }
-    console.log(email);
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://root:kXrFk778zMRXyu8w@cluster0.sr7nv.mongodb.net/?retryWrites=true&w=majority`
+    );
+
+    const db = client.db("newsletter");
+
+    await db.collection("emails").insertOne({ email });
+
+    client.close();
 
     res.status(201).json({ message: "Signed up" });
   }
