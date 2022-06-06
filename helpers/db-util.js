52cf6760c0359056dd5c8aba90e1c675b9484548
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-06 12:05:43 +0100

    add db-util

---
+++ helpers/db-util.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  return await MongoClient.connect(
+    `mongodb+srv://root:kXrFk778zMRXyu8w@cluster0.sr7nv.mongodb.net/events?retryWrites=true&w=majority`
+  );
+}
+
+export async function inserDocument(client, collection, document) {
+  const db = client.db();
+
+  return await db.collection(collection).insertOne(document);
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  return await client.collection(collection).find().sort(sort).toArray();
+}
