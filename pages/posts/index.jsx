233fcd29670a43b4cd221601cfc1623b84e0ab3b
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-07 12:04:37 +0100

    load all posts from md files

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,6 +1,10 @@
 import AllPosts from "../../components/posts/all-posts";
-import { DUMMY_POSTS } from "..";
+import { getAllPosts } from "../../helpers/posts";
 
-export default function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export default function AllPostsPage(props) {
+  return <AllPosts {...props} />;
+}
+
+export function getStaticProps() {
+  return { props: { posts: getAllPosts() } };
 }
