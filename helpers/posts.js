2b52c246ea937a17cf4be71ab4e68f52d7b07fdc
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-07 11:50:14 +0100

    post helper

---
+++ helpers/posts.js
@@ -0,0 +1,26 @@
+import matter from "gray-matter";
+
+import fs from "fs";
+import path from "path";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+function getPostData(fileName) {
+  const filePath = path.join(postsDirectory, fileName);
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(fileContent);
+
+  return { slug: fileName.replace(/\.md$/, ""), ...data, content };
+}
+
+export function getAllPosts() {
+  const postFiles = fs.readdirSync(postsDirectory);
+
+  const allPosts = postFiles.map((postFile) => getPostData(postFile));
+
+  return allPosts.sort((postA, postB) => (postA.date > postB.date ? -1 : 1));
+}
+
+export function getFeaturedPosts() {
+  return getAllPosts().filter((post) => post.isFeatured);
+}
