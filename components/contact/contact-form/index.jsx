2224970e3aff6da9e318cdff5b4660d1679ec9b3
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-07 18:28:36 +0100

    add notification

--- components/contact/contact-form/index.jsx
+++ components/contact/contact-form/index.jsx
@@ -1,19 +1,51 @@
 import { useState } from "react";
+import Notification from "../../ui/notification";
 import classes from "./styles.module.css";
 
 export default function ContactForm() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+  const [requestError, setRequestError] = useState();
 
-  function sendMessageHandler(event) {
-    event.preventDefault();
-    fetch("/api/contact", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, name, message }),
-    });
+  async function sendMessageHandler(e) {
+    e.preventDefault();
+
+    setRequestStatus("pending");
+
+    try {
+      await sendContactData({ email, name, message });
+      setRequestStatus("success");
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus("error");
+    }
   }
+
+  let notification;
+
+  if (requestStatus === "pending")
+    notification = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is on its way!",
+    };
+
+  if (requestStatus === "success")
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: "Message sent successfully!",
+    };
+
+  if (requestStatus === "error")
+    notification = {
+      status: "error",
+      title: "Error!",
+      message: requestError,
+    };
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
@@ -56,6 +88,20 @@ export default function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && <Notification {...notification} />}
     </section>
   );
+}
+async function sendContactData(contactDetails) {
+  const res = await fetch("/api/contact", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(contactDetails),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.messge || "Something went wrong!");
+  }
 }
