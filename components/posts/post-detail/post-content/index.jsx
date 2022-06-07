3ae15544c1f51064fa27de839211fd1f3c4ab1b4
Geomar Bastiani <mail@geomarbastiani.com>
2022-06-07 12:34:28 +0100

    fix: single post content

--- components/posts/post-detail/post-content/index.jsx
+++ components/posts/post-detail/post-content/index.jsx
@@ -3,8 +3,10 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "../post-header";
 import classes from "./styles.module.css";
 
-export default function PostContent({ slug, title, image, content }) {
+export default function PostContent({ post }) {
+  const { slug, title, image, content } = post;
   const imagePath = `/images/posts/${slug}/${image}`;
+
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
